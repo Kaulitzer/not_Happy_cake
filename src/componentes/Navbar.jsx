f96cb50ea2766d821994f09cf6d7416d0a04c988
kaulitzer <kaulitzer001@gmail.com>
2023-09-25 20:32:15 -0300

    se agrega logo

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import iconoImagen from '../assets/happycake_icon.jpg';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="nav-item">
           <Link to="/contacto">Contacto</Link>
         </li>
       </ul>
-      <div className="logo">Happy Cake</div>
+      <div className="logo">
+        <img src={iconoImagen} alt="Icono" className="icon" />
+        Happy Cake
+      </div>
     </nav>
   );
 }
